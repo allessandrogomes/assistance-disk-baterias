0122f4193f18b57c1ddefa318177c2f15494d8ad
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-03-25 20:05:41 -0300

    feat: adiciona a nova rota 'LoanBatteries'.

--- src/renderer/src/routes.jsx
+++ src/renderer/src/routes.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import ReturnToCustomers from "./pages/ReturnToCustomers";
 import DefaultPage from "./components/DefaultPage";
+import LoanBatteries from "./pages/LoanBatteries";
 
 
 export default function Router() {
@@ -11,6 +12,7 @@ export default function Router() {
                 <Route path='/' element={<DefaultPage />}>
                     <Route index element={<Home />} />
                     <Route path='/retorno' element={<ReturnToCustomers />} />
+                    <Route path='/baterias-de-emprestimo' element={<LoanBatteries />} />
                 </Route>
             </Routes>
         </BrowserRouter>
