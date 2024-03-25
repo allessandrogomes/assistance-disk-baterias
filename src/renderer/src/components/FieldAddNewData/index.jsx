97b91444dea947a869f81354600aed0726976e1b
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-03-25 19:56:40 -0300

    style: altera a identação dos TextFields do fórumulário, para melhor visualização.

--- src/renderer/src/components/FieldAddNewData/index.jsx
+++ src/renderer/src/components/FieldAddNewData/index.jsx
@@ -10,6 +10,8 @@ export default function FieldAddNewData({ requestRegisteredSuccessfully }) {
 
     const requests = useSelector(state => state.requests)
 
+    const dispatch = useDispatch()
+
     const [formData, setFormData] = useState({
         request: '',
         clientName: '',
@@ -72,8 +74,6 @@ export default function FieldAddNewData({ requestRegisteredSuccessfully }) {
         return format(date, 'dd/MM/yyyy');
     }
 
-    const dispatch = useDispatch()
-
     const handleSubmit = (event) => {
         event.preventDefault()
 
@@ -99,16 +99,110 @@ export default function FieldAddNewData({ requestRegisteredSuccessfully }) {
         <form onSubmit={handleSubmit}>
             <FormControl sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '20px', maxWidth: '95vw', bgcolor: '#EEEEEE', p: '10px' }}>
                 <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: '10px', justifyContent: 'center', width: '100%' }}>
-                    <TextField onChange={updateFormData} inputProps={{ maxLength: 4 }} id="request" name="request" size="small" label="N° REQUISIÇÃO" value={formData.request} variant="filled" required />
-                    <TextField onChange={updateFormData} id="clientName" name="clientName" size="small" label="Nome do cliente" value={formData.clientName} variant="filled" required />
-                    <TextField onChange={updateFormData} inputProps={{ maxLength: 11 }} id="cpf" name="cpf" size="small" label="CPF" value={formData.cpf} variant="filled" />
-                    <TextField onChange={updateFormData} inputProps={{ maxLength: 11 }} id="phoneNumber" name="phoneNumber" size="small" label="Telefone" value={formData.phoneNumber} variant="filled" required />
-                    <TextField onChange={updateFormData} inputProps={{ max: maxDate.toISOString().split("T")[0] }} id="entryDate" name="entryDate" type="date" size="small" label="Data de entrada" value={formData.entryDate} variant="filled" required />
-                    <TextField onChange={updateFormData} inputProps={{ max: maxDate.toISOString().split("T")[0] }} id="returnDate" name="returnDate" type="date" size="small" label="Data de retorno" value={formData.returnDate} variant="filled" required />
-                    <TextField onChange={updateFormData} id="batteryModel" name="batteryModel" size="small" label="Modelo da bateria" value={formData.batteryModel} variant="filled" required />
-                    <TextField onChange={updateFormData} id="batteryCode" name="batteryCode" size="small" label="Código da bateria" value={formData.batteryCode} variant="filled" required />
-                    <TextField onChange={updateFormData} id="loanBatteryModel" name="loanBatteryModel" size="small" label="Modelo bateria empréstimo" value={formData.loanBatteryModel} variant="filled" />
-                    <TextField onChange={updateFormData} id="loanBatteryCode" name="loanBatteryCode" size="small" label="Código bateria empréstimo" value={formData.loanBatteryCode} variant="filled" />
+                    <TextField
+                        onChange={updateFormData}
+                        inputProps={{ maxLength: 4 }}
+                        id="request"
+                        name="request"
+                        size="small"
+                        label="N° REQUISIÇÃO"
+                        value={formData.request}
+                        variant="filled"
+                        required
+                    />
+                    <TextField
+                        onChange={updateFormData}
+                        id="clientName"
+                        name="clientName"
+                        size="small"
+                        label="Nome do cliente"
+                        value={formData.clientName}
+                        variant="filled"
+                        required
+                    />
+                    <TextField
+                        onChange={updateFormData}
+                        inputProps={{ maxLength: 11 }}
+                        id="cpf"
+                        name="cpf"
+                        size="small"
+                        label="CPF"
+                        value={formData.cpf}
+                        variant="filled"
+                    />
+                    <TextField
+                        onChange={updateFormData}
+                        inputProps={{ maxLength: 11 }}
+                        id="phoneNumber"
+                        name="phoneNumber"
+                        size="small"
+                        label="Telefone"
+                        value={formData.phoneNumber}
+                        variant="filled"
+                        required
+                    />
+                    <TextField
+                        onChange={updateFormData}
+                        inputProps={{ max: maxDate.toISOString().split("T")[0] }}
+                        id="entryDate"
+                        name="entryDate"
+                        type="date"
+                        size="small"
+                        label="Data de entrada"
+                        value={formData.entryDate}
+                        variant="filled"
+                        required
+                    />
+                    <TextField
+                        onChange={updateFormData}
+                        inputProps={{ max: maxDate.toISOString().split("T")[0] }}
+                        id="returnDate"
+                        name="returnDate"
+                        type="date"
+                        size="small"
+                        label="Data de retorno"
+                        value={formData.returnDate}
+                        variant="filled"
+                        required
+                    />
+                    <TextField
+                        onChange={updateFormData}
+                        id="batteryModel"
+                        name="batteryModel"
+                        size="small"
+                        label="Modelo da bateria"
+                        value={formData.batteryModel}
+                        variant="filled"
+                        required
+                    />
+                    <TextField
+                        onChange={updateFormData}
+                        id="batteryCode"
+                        name="batteryCode"
+                        size="small"
+                        label="Código da bateria"
+                        value={formData.batteryCode}
+                        variant="filled"
+                        required
+                    />
+                    <TextField
+                        onChange={updateFormData}
+                        id="loanBatteryModel"
+                        name="loanBatteryModel"
+                        size="small"
+                        label="Modelo bateria empréstimo"
+                        value={formData.loanBatteryModel}
+                        variant="filled"
+                    />
+                    <TextField
+                        onChange={updateFormData}
+                        id="loanBatteryCode"
+                        name="loanBatteryCode"
+                        size="small"
+                        label="Código bateria empréstimo"
+                        value={formData.loanBatteryCode}
+                        variant="filled"
+                    />
                 </Box>
                 <Button type='submit' variant="contained">Adicionar</Button>
             </FormControl>
