cde0b08ac3e852789a46514dba497b27a36de2d8
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-03-25 20:06:47 -0300

    feat: adiciona o novo reducer 'loanBatteries' ao store.

--- src/renderer/src/store/index.js
+++ src/renderer/src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import requestsSlice from "./reducers/requests";
+import loanBatteriesSlice from "./reducers/loanBatteries";
 
 
 const store = configureStore({
     reducer: {
-        requests: requestsSlice
+        requests: requestsSlice,
+        loanBatteries: loanBatteriesSlice
     }
 })
 
