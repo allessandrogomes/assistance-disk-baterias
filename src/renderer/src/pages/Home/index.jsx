6d299e07bf73c98c5df00246134da2f2b09bfc7e
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-03-24 23:48:40 -0300

    refactor: altera o field 'id' da table para 'request' e adiciona a key personalizada 'batteryCode' para as celulas da tabela por meio do 'getRowId'.

--- src/renderer/src/pages/Home/index.jsx
+++ src/renderer/src/pages/Home/index.jsx
@@ -1,7 +1,8 @@
-import { Box, Button } from "@mui/material"
+import { Alert, Box, Button } from "@mui/material"
 import FieldAddNewData from "../../components/FieldAddNewData"
 import DataTable from "../../components/Table"
 import { useNavigate } from "react-router-dom"
+import { useEffect, useState } from "react"
 
 
 
@@ -9,10 +10,28 @@ function Home() {
 
   const navigate = useNavigate()
 
+  const [requestRegisteredSuccessfully, setRequestRegisteredSuccessfully] = useState(undefined)
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setRequestRegisteredSuccessfully(null)
+    }, 5000)
+
+    return () => clearTimeout(timeoutId)
+  }, [requestRegisteredSuccessfully])
+
   return (
     <Box sx={{ alignItems: 'center', display: 'flex', flexDirection: 'column', gap: '50px', justifyContent: 'center' }}>
       <Button onClick={() => navigate('/retorno')}>Tela de retorno</Button>
-      <FieldAddNewData />
+      {
+        requestRegisteredSuccessfully === true ? (
+          <Alert severity="success">Requisição cadastrada com sucesso.</Alert>
+        ) : requestRegisteredSuccessfully === false ? (
+          <Alert severity="warning">Já existe uma bateria com esse código cadastrado, por favor tente novamente.</Alert>
+        ) : null
+      }
+      <FieldAddNewData requestRegisteredSuccessfully={value => setRequestRegisteredSuccessfully(value)}
+      />
       <DataTable />
     </Box>
   )
