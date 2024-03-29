d246c3280693eeb58e11379b40c62d421370328b
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-03-29 19:03:05 -0300

    remove: a responsabilidade de atualizar em 'requests' o 'numberOfDaysPassed' e 'daysOfDelay', os botões de navegação e o botão de atualizar sistema

--- src/renderer/src/pages/ReturnToCustomers/index.jsx
+++ src/renderer/src/pages/ReturnToCustomers/index.jsx
@@ -7,51 +7,21 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Box, Button, Typography } from '@mui/material';
-import RefreshIcon from '@mui/icons-material/Refresh';
 import { useDispatch, useSelector } from 'react-redux';
-import { updateData } from '../../store/reducers/requests';
 import { useNavigate } from 'react-router-dom';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import { useEffect } from 'react';
+import { updateData } from '../../store/reducers/requests';
 
 export default function ReturnToCustomers() {
 
     const requests = useSelector(state => state.requests)
 
-    const dispatch = useDispatch()
     const navigate = useNavigate()
-
-    const updateTable = () => {
-        const updatedRequests = requests.map(request => ({ ...request }))
-
-        updatedRequests.forEach(request => {
-            request.numberOfDaysPassed = definesNumberOfDaysPassed(request.entryDate)
-            request.daysOfDelay = request.numberOfDaysPassed - request.deadlineDays
-        })
-
-        dispatch(updateData(updatedRequests))
-    }
-
-    const definesNumberOfDaysPassed = (entryDate) => {
-        const todayDate = new Date()
-
-        const [day, month, year] = entryDate.split('/')
-        const entryDateInDateFormat = new Date(year, month - 1, day, 0, 0, 0)
-
-        const differenceInMilliseconds = Math.abs(todayDate - entryDateInDateFormat)
-
-        const differenceInDays = Math.ceil(differenceInMilliseconds / (1000 * 60 * 60 * 24))
-
-        return differenceInDays - 1
-    }
 
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '20px' }}>
-            <Box>
-                <Button onClick={() => navigate('/')}>Início</Button>
-                <Button onClick={() => navigate('/baterias-de-emprestimo')}>Baterias de empréstimo</Button>
-            </Box>
             <Typography sx={{ fontSize: '2rem', fontWeight: 'bold' }}>Retorno aos clientes</Typography>
-            <Button onClick={updateTable}>Atualizar<RefreshIcon /></Button>
             <TableContainer sx={{ width: "95vw" }} component={Paper}>
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
                     <TableHead>
