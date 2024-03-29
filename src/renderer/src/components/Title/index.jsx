b4ce35f51d49c007a8c3edaa73bb01bc00337391
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-03-29 18:21:56 -0300

    feat: atualiza a estilização do componente 'Title'

--- src/renderer/src/components/Title/index.jsx
+++ src/renderer/src/components/Title/index.jsx
@@ -2,7 +2,7 @@ import { Box, Typography } from "@mui/material";
 
 export default function Title() {
     return (
-        <Box sx={{ mt: '50px' }}>
+        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#31363F', height: '125px', width: '100%' }}>
             <Typography
                 component="h1"
                 sx={{ fontSize: '2rem', textTransform: 'uppercase', fontWeight: 'bold' }}
