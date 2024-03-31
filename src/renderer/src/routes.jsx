2a7d7598407e0bfbf8624577f4e6aa810d77aa44
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-03-31 20:43:22 -0300

    feat: adiciona as rotas 'editar', 'editar/requisicoes' e 'editar/baterias-de-emprestimo'

--- src/renderer/src/routes.jsx
+++ src/renderer/src/routes.jsx
@@ -3,6 +3,9 @@ import Home from "./pages/Home";
 import ReturnToCustomers from "./pages/ReturnToCustomers";
 import DefaultPage from "./components/DefaultPage";
 import LoanBatteries from "./pages/LoanBatteries";
+import Edition from "./pages/Edition";
+import EditionRequests from "./pages/EditionRequests";
+import EditionLoanBatteries from "./pages/EditionLoanBatteries";
 
 
 export default function Router() {
@@ -13,6 +16,9 @@ export default function Router() {
                     <Route index element={<Home />} />
                     <Route path='/retorno' element={<ReturnToCustomers />} />
                     <Route path='/baterias-de-emprestimo' element={<LoanBatteries />} />
+                    <Route path='/editar' element={<Edition />} />
+                    <Route path='/editar/requisicoes' element={<EditionRequests />} />
+                    <Route path='/editar/baterias-de-emprestimo' element={<EditionLoanBatteries />} />
                 </Route>
             </Routes>
         </BrowserRouter>
