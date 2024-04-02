0b1f93bfdc11e747e44299e80833764a3b460a84
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-04-02 20:09:57 -0300

    style: remove uma linha desnecessária.

--- electron.vite.config.mjs
+++ electron.vite.config.mjs
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
   },
   preload: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
   },
   renderer: {
     resolve: {
