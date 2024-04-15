d81e0451701c64c1fd6c837c0fc40b0c0e5effa2
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-04-15 19:13:23 -0300

    feat: cria as funções que serão utilizadas na renderer(front end) para executar a leitura e escrita nos arquivos json.

--- src/preload/index.js
+++ src/preload/index.js
@@ -1,15 +1,56 @@
-import { contextBridge } from 'electron'
-import { electronAPI } from '@electron-toolkit/preload'
+import { contextBridge, ipcRenderer } from 'electron'
 
-const api = {}
-if (process.contextIsolated) {
-  try {
-    contextBridge.exposeInMainWorld('electron', electronAPI)
-    contextBridge.exposeInMainWorld('api', api)
-  } catch (error) {
-    console.error(error)
-  }
-} else {
-  window.electron = electronAPI
-  window.api = api
+let saveDataRequests = (data) => {
+  ipcRenderer.send("saveDataRequests", data)
 }
+
+let readDataRequests = () => {
+  return new Promise((resolve, reject) => {
+    ipcRenderer.send('readDataRequests')
+
+    ipcRenderer.once('readDataRequestsResponse', (event, response) => {
+      if (response.error) {
+        console.error('Erro ao ler os dados:', response.error)
+        reject(response.error)
+      } else {
+        const jsonData = response.data
+        resolve(jsonData)
+      }
+    })
+  })
+}
+
+let bridgeRequests = {
+  saveDataRequests,
+  readDataRequests
+}
+
+contextBridge.exposeInMainWorld('bridgeRequests', bridgeRequests)
+
+
+let saveDataLoanBatteries = (data) => {
+  ipcRenderer.send("saveDataLoanBatteries", data)
+}
+
+let readDataLoanBatteries = () => {
+  return new Promise((resolve, reject) => {
+    ipcRenderer.send('readDataLoanBatteries')
+
+    ipcRenderer.once('readDataLoanBatteriesResponse', (event, response) => {
+      if (response.error) {
+        console.error('Erro ao ler os dados:', response.error)
+        reject(response.error)
+      } else {
+        const jsonData = response.data
+        resolve(jsonData)
+      }
+    })
+  })
+}
+
+let bridgeLoanBatteries = {
+  saveDataLoanBatteries,
+  readDataLoanBatteries
+}
+
+contextBridge.exposeInMainWorld('bridgeLoanBatteries', bridgeLoanBatteries)
