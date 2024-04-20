c9cd0748a7e376a7e1adec0546782d36aecc7d57
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-04-19 22:43:20 -0300

    :bug:fix: fixa o bug de não renderizar corretamente o react router em alguns computadores, substituindo 'HashRouter' por 'MemoryRouter'.

--- src/renderer/src/routes.jsx
+++ src/renderer/src/routes.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, MemoryRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import ReturnToCustomers from "./pages/ReturnToCustomers";
 import DefaultPage from "./components/DefaultPage";
@@ -11,18 +11,18 @@ import RequestOutput from "./pages/RequestOutput";
 
 export default function Router() {
     return (
-        <HashRouter>
+        <MemoryRouter>
             <Routes>
-                <Route path='/' element={<DefaultPage />}>
+                <Route path="/" element={<DefaultPage />}>
                     <Route index element={<Home />} />
-                    <Route path='/saida' element={<RequestOutput />} />
-                    <Route path='/retorno' element={<ReturnToCustomers />} />
-                    <Route path='/baterias-de-emprestimo' element={<LoanBatteries />} />
-                    <Route path='/editar' element={<Edition />} />
-                    <Route path='/editar/requisicoes' element={<EditionRequests />} />
-                    <Route path='/editar/baterias-de-emprestimo' element={<EditionLoanBatteries />} />
+                    <Route path="/saida" element={<RequestOutput />} />
+                    <Route path="/retorno" element={<ReturnToCustomers />} />
+                    <Route path="/baterias-de-emprestimo" element={<LoanBatteries />} />
+                    <Route path="/editar" element={<Edition />} />
+                    <Route path="/editar/requisicoes" element={<EditionRequests />} />
+                    <Route path="/editar/baterias-de-emprestimo" element={<EditionLoanBatteries />} />
                 </Route>
             </Routes>
-        </HashRouter>
+        </MemoryRouter>
     )
 }
