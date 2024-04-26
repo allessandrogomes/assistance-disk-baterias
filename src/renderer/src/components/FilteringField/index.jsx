2ea892fb4b42e9ca902bacd3d7c295a2e6cc1d48
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-04-25 22:52:57 -0300

    :recycle:refactor: personaliza por completo o texto da label do campo de filtragem.

--- src/renderer/src/components/FilteringField/index.jsx
+++ src/renderer/src/components/FilteringField/index.jsx
@@ -1,10 +1,10 @@
 import { Box, Input, InputLabel } from "@mui/material";
 
 
-export default function FilteringField({ inputLabelFilterBy, onChangeValue, inputValue }) {
+export default function FilteringField({ labelText, onChangeValue, inputValue }) {
     return (
-        <Box sx={{ backgroundColor: '#EEEEEE', borderRadius: '5px', p: '10px', mb: '20px', width: '250px' }}>
-            <InputLabel>Filtrar pelo {inputLabelFilterBy}</InputLabel>
+        <Box sx={{ backgroundColor: '#EEEEEE', borderRadius: '5px', p: '10px', width: '250px' }}>
+            <InputLabel>{labelText}</InputLabel>
             <Input onChange={(event) => onChangeValue(event.target.value)} value={inputValue} sx={{ width: '70px' }} />
         </Box>
     )
